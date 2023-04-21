8f6021a6ac28d3dcaff5a6b5ff9047ba7c01203b
Damiro Souza <97421651+damirojr@users.noreply.github.com>
2023-04-21 05:26:30 -0300

    Bug-fix manifest deployment

--- aks-lab/outputs.tf
+++ aks-lab/outputs.tf
@@ -2,13 +2,13 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
-  sensitive = true
-}
+#output "client_certificate" {
+#  value     = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+#  sensitive = true
+#}
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
-
-  sensitive = true
-}
+#output "kube_config" {
+#  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+#
+#  sensitive = true
+#}
